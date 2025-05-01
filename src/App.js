47f86a43353e3a64d28f1d72599c64a6a8accd30
harshpanchal21 <harshpanchal19@gnu.ac.in>
2025-05-01 14:20:26 +0530

    Added Signup page to GO-FX

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-
-import { Navbar } from './components';
-import { Home, Stocks, StockDetails, Crypto, CryptoDetails, Coins, CoinDetails, Exchanges, ExchangeDetails, Commodities, Forex, ForexDetails, Strategies, StrategyDetails } from './pages'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
 
 function App() {
-
-
   return (
-    <div className="text-center">
-      <header className="">
-        <Navbar />
-      </header>
-      <div>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/stocks' element={<Stocks />} />
-          <Route path='/stocks/:stockSymbol' element={<StockDetails />} />
-          {/* <Route path='/stocks/:stockId' element={<StockDetails />} /> */}
-          <Route path='/crypto' element={<Crypto />} />
-          <Route path='/crypto/:cryptoSymbol' element={<CryptoDetails />} />
-          <Route path='/coins' element={<Coins />} />
-          <Route path='/coins/:coinSymbol' element={<CoinDetails />} />
-          <Route path='/exchanges' element={<Exchanges />} />
-          <Route path='/exchanges/:exchangeId' element={<ExchangeDetails />} />          
-          <Route path='/commodities' element={<Commodities />} />
-          <Route path='/forex' element={<Forex />} />
-          <Route path='/forex/:forexPair' element={<ForexDetails />} />
-          <Route path='/strategies' element={<Strategies />} />
-          <Route path='/strategies/:strategyCode' element={<StrategyDetails />} />
-        </Routes>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        {/* existing routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
