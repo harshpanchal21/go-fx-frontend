6fafbb26a07b21f3b93d411132f12ff958525e3b
harshpanchal21 <harshpanchal19@gnu.ac.in>
2025-05-01 14:28:03 +0530

    Added Signup page to GO-FX

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'; // or replace with working component
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* existing routes */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
